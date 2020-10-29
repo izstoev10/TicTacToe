cdb3a833ee59854ffaad490583d3bd29cc85c730
izstoev10 <ivan.zstoev@gmail.com>
2020-10-29 00:55:34 +0000

    unit test

--- TicTacToc.Tests/CheckWinnerTest.cs
+++ TicTacToc.Tests/CheckWinnerTest.cs
@@ -10,21 +10,159 @@ namespace TicTacToe.Tests
     public class CheckWinnerTests    
     {
         [Test]
-        public void CheckWinnerIsTrue()
+        public void AssumeFirstHorizontalIsTrue()
         {
             //Arrange
             var winner = new Game();
 
-            //Act   
-            //var checkForWin = winner.CheckForWinner();
-
+            winner.boardArray[0, 0] = (int)MarkType.Cross;
+            winner.boardArray[0, 1] = (int)MarkType.Cross;
+            winner.boardArray[0, 2] = (int)MarkType.Cross;
 
             //Assert     
-            Assert.That(winner.boardArray[0, 0], Is.EqualTo((int)MarkType.Free));
-            Assert.That(winner.boardArray[0, 1], Is.EqualTo((int)MarkType.Free));
-            Assert.That(winner.boardArray[0, 2], Is.EqualTo((int)MarkType.Free));
+            Assert.AreEqual(winner.boardArray[0, 0], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[0, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[0, 2], (int)MarkType.Cross);
             Assert.IsTrue(winner.isGameFinished = true);
         }
+
+        [Test]
+        public void AssumeSecondHorizontalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[1, 0] = (int)MarkType.Cross;
+            winner.boardArray[1, 1] = (int)MarkType.Cross;
+            winner.boardArray[1, 2] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.AreEqual(winner.boardArray[1, 0], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 2], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeThirdHorizontalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[2, 0] = (int)MarkType.Cross;
+            winner.boardArray[2, 1] = (int)MarkType.Cross;
+            winner.boardArray[2, 2] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.AreEqual(winner.boardArray[2, 0], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[2, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[2, 2], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeFirstVerticalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[0, 0] = (int)MarkType.Cross;
+            winner.boardArray[1, 0] = (int)MarkType.Cross;
+            winner.boardArray[2, 0] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.AreEqual(winner.boardArray[0, 0], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 0], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[2, 0], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeSecondVerticalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[0, 1] = (int)MarkType.Cross;
+            winner.boardArray[1, 1] = (int)MarkType.Cross;
+            winner.boardArray[2, 1] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.AreEqual(winner.boardArray[0, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[2, 1], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeThirdVerticalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[0, 2] = (int)MarkType.Cross;
+            winner.boardArray[1, 2] = (int)MarkType.Cross;
+            winner.boardArray[2, 2] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.AreEqual(winner.boardArray[0, 2], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 2], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[2, 2], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeFirstDiagonalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[0, 0] = (int)MarkType.Cross;
+            winner.boardArray[1, 1] = (int)MarkType.Cross;
+            winner.boardArray[2, 2] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.That(winner.boardArray[0, 0] == (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[2, 2], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeSecondDiagonalIsTrue()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[2, 0] = (int)MarkType.Cross;
+            winner.boardArray[1, 1] = (int)MarkType.Cross;
+            winner.boardArray[0, 2] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.That(winner.boardArray[2, 0] == (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[1, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[0, 2], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+        [Test]
+        public void AssumeThereIsNoWinner()
+        {
+            //Arrange
+            var winner = new Game();
+
+            winner.boardArray[0, 0] = (int)MarkType.Cross;
+            winner.boardArray[0, 1] = (int)MarkType.Cross;
+            winner.boardArray[0, 2] = (int)MarkType.Cross;
+
+            //Assert     
+            Assert.That(winner.boardArray[0, 0] == (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[0, 1], (int)MarkType.Cross);
+            Assert.AreEqual(winner.boardArray[0, 2], (int)MarkType.Cross);
+            Assert.IsTrue(winner.isGameFinished = true);
+        }
+
+
     }
 }
 
